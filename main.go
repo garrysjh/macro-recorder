ab72a21600faa175054800a1c0be870448d1c746
garrysjh <garrysjh@gmail.com>
2024-12-11 17:22:47 +0800

    basic wasd keylog

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Session struct{}
+
+func NewSession() *Session {
+	return &Session{}
+}
+
+func (s *Session) Update() error {
+	keyLog(s)
+	return nil
+}
+
+func (s *Session) Draw(screen *ebiten.Image) {
+}
+
+func (s *Session) Layout(w, h int) (int, int) {
+	return 1920, 1280
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	s := NewSession()
+	if err := ebiten.RunGame(s); err != nil {
+		fmt.Println("err")
+	}
+}
+
+func keyLog(s *Session) {
+	if ebiten.IsKeyPressed(ebiten.KeyW) {
+		fmt.Println("W")
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyA) {
+		fmt.Println("A")
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyS) {
+		fmt.Println("S")
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyD) {
+		fmt.Println("D")
+	}
 }
